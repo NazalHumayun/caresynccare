1b73fab24e32e6f42908010d87fa2030672eb7b5
NazalHumayun <naughtnorma@gmail.com>
2026-06-04 10:05:50 +0500

    Update axiosinstance.js

--- src/api/axiosinstance.js
+++ src/api/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||"https://caresync-care-production.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL ||"https://caresync-care-nine.vercel.app",
 });
 
 axiosInstance.interceptors.request.use((config) => {
