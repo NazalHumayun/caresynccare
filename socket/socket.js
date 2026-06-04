086e067c42fdada860aeb3dada8430ac3f478f6d
NazalHumayun <naughtnorma@gmail.com>
2026-06-04 10:48:21 +0500

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://caresync-care-nine.vercel.app" {
+const socket = io("https://caresync-care-nine.vercel.app" , {
   transports: ["websocket"], // ✅ force websocket only
   withCredentials: true
 });
